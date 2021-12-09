584143faca51803473b78d1a2bfb6e3b2d1526d9
Hash466 <y.h466h@gmail.com>
2021-12-09 23:15:37 +0300

    small fix

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
 from .models import User
 from .serializers import UserSerializer
@@ -17,11 +18,11 @@ class UserViewSet(CreateListRetrieveViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    # @action(detail=True)
-    # def me(self, request):
-    #     user = User.objects.get(username=request.username)
-    #     serializer = self.get_serializer(user)
-    #     return Response(serializer.data)
+    @action(detail=True)
+    def me(self, request):
+        user = User.objects.get_object_or_404(username=request.user.username)
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
 
     # @action(methods=['post'], detail=True)
     # def set_password(self, request):
